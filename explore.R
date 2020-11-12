a424d8dca0647a5d58519b4fb91e70bbeb74ece2
AstrobioMike <michael.lee0517@gmail.com>
2020-11-12 12:53:44 -0800

    update

--- explore.R
+++ explore.R
@@ -19,17 +19,16 @@ load("BRAILLE-mg.RData")
    # nitrogen: N_KO_gene_df_list
    # methane:  methane_KO_gene_df_list
    # sulfur:   sulfur_KO_gene_df_list
+   # carbon:   carbon_KO_gene_df_list
 
 # tables with KO information for each group are available in the browser at the bottom right under the "Files" tab
 
 # e.g. to plot K10946	pmoC-amoC	methane/ammonia monooxygenase subunit C from the nitrogem metabolism overview
 plot_single_KO("K10946", N_KO_gene_df_list)
 
-# or K11180	dsrA	dissimilatory sulfite reductase alpha subunit from the sulfur metabolism overview
-plot_single_KO("K11180", sulfur_KO_gene_df_list)
-  # that gives a bunch of warnings and empty plot
-  # can check the counts like so:
-sulfur_KO_gene_df_list[["counts"]] %>% filter(KO_ID == "K11180")
-  # seems there are no hits for that one
+# if we check for something that wasn't detected, we'll get a message telling us that
+# e.g. K02588	nifH	nitrogenase iron protein NifH
+plot_single_KO("K02588", N_KO_gene_df_list)
+
 
 # will update this soon with more functionality and examples
